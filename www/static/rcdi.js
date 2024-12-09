fae0d2e7ea6dd6f6936f24a0799f6c42d2a09e47
Keneth Rodriguez <kenethlaunies.rodriguez@alu.uclm.es>
2024-12-09 22:58:24 +0000

    fixed correct libraries

--- www/static/rcdi.js
+++ www/static/rcdi.js
@@ -52,7 +52,7 @@ const conn = io(); //socketio connection to server//
 			switch(msg.source){
 				case 'computer':
 					if(msg.action=='useCursor' && MY_WS_ID==msg.targetID){
-						window.open('/local/php/mouse.php','_self');
+						window.open('/php/mouse.php','_self');
 					}
 					if(msg.action=='stopCursor' && MY_WS_ID==msg.targetID){
 						window.open('/','_self');
@@ -327,7 +327,7 @@ function startUsingWebCursor(){
 }
 
 function startUsingThisWebCursor(){
-	window.open('/local/php/mouse.php','_self');
+	window.open('/php/mouse.php','_self');
 }
 
 function stopUsingWebCursor(){
