e295e6f447f5fe77d84e8edafd54ec0885f51252
Keneth Rodriguez <kenethlaunies.rodriguez@alu.uclm.es>
2024-12-10 00:10:09 +0000

    Changed libraries to flask_sock from socketio.

--- backend_testbed/myapp.py
+++ backend_testbed/myapp.py
@@ -1,12 +1,12 @@
 from flask import Flask,render_template,request
-from flask_socketio import SocketIO, emit
+from flask_sock import Sock
 import subprocess
 from werkzeug.middleware.proxy_fix import ProxyFix
 from enum import Enum
 
 app = Flask(__name__)
+sock=Sock(app)
 app.wsgi_app = ProxyFix(app.wsgi_app,x_for=1, x_proto=1, x_host=1, x_prefix=1)
-socketio = SocketIO(app,debug=True,cors_allowed_origins='*',async_mode='eventlet')
 
 class Events(Enum):
     PING = 1
@@ -22,17 +22,7 @@ def write_log(data):
     with open('log.txt','a') as f:
         f.write(data+'\n')
 
-
-@app.route('/home')
-def main():
-        return render_template('base.html')
-
-# Print contents of message received on the websocket
-@socketio.on('connect')
-def register_connection(json):
-    write_log('received json: ' + str(json) + ' from ' + request.sid + '\n')
-
-
-@socketio.on('my_event')
-def handle_custom_event(json):
-    write_log('received json my_event: ' + str(json) + ' from ' + request.sid + '\n')
+@sock.route('/socket.io')
+def socketio():
+    data=sock.receive()
+    write_log(data)
