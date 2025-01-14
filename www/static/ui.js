f60eca75f2b62b59267dc700d9ebc9fb6a0bc0e7
Keneth Rodriguez <kenethlaunies.rodriguez@alu.uclm.es>
2025-01-14 20:59:16 +0000

    testing login with microsoft

---
+++ www/static/ui.js
@@ -0,0 +1,15 @@
+// Select DOM elements to work with
+const signInButton = document.getElementById('signIn');
+const signOutButton = document.getElementById('signOut');
+const titleDiv = document.getElementById('title-div');
+const welcomeDiv = document.getElementById('welcome-div');
+const tableDiv = document.getElementById('table-div');
+const tableBody = document.getElementById('table-body-div');
+
+function welcomeUser(username) {
+    signInButton.classList.add('d-none');
+    signOutButton.classList.remove('d-none');
+    titleDiv.classList.add('d-none');
+    welcomeDiv.classList.remove('d-none');
+    welcomeDiv.innerHTML = `Welcome ${username}!`;
+};
