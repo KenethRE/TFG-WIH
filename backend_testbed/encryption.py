074197ff399671d7470f89ea832c9e00bde08f25
Keneth Rodriguez <kenethlaunies.rodriguez@alu.uclm.es>
2025-06-15 09:44:17 +0000

    updated encryption libraries

--- backend_testbed/encryption.py
+++ backend_testbed/encryption.py
@@ -1,35 +1,26 @@
-from cryptography.fernet import Fernet
-secrets_file = '/root/flask_key.key'
+import secrets
+from flask_bcrypt import Bcrypt
+from pathlib import Path
+secrets_file = '/root/.flask_key.key'
 
 # This code is used to handle encryption and decryption of sensitive data
 # Ensure the key is read correctly and strip any whitespace
 
-
 def get_secrets():
-    f = open(secrets_file, 'r')
-    string_key = f.read().strip()
-    f.close()
-    if not string_key:
-        raise ValueError("The key file is empty or not found.")
-    fernet = Fernet(string_key.encode())
-    return fernet.decrypt("gAAAABoTUsfhd-ZppGmDrpHBdKeYKiM9Q-LBneUuAi53EKGfM-dBUbuF3FtXMdIvoYWGJVos_Xr9kn9TBnmpCrriP2THfctmSzC0tCD9EoZyUloq9S6u4w=".encode()).decode()
-
-def encrypt_data(data):
-    f = open(secrets_file, 'r')
-    string_key = f.read().strip()
-    f.close()
-    if not string_key:
-        raise ValueError("The key file is empty or not found.")
-    fernet = Fernet(string_key.encode())
-    encrypted_data = fernet.encrypt(data.encode())
-    return encrypted_data.decode()
-
-def decrypt_data(encrypted_data):
-    f = open(secrets_file, 'r')
-    string_key = f.read().strip()
-    f.close()
-    if not string_key:
-        raise ValueError("The key file is empty or not found.")
-    fernet = Fernet(string_key.encode())
-    decrypted_data = fernet.decrypt(encrypted_data.encode())
-    return decrypted_data.decode()
+    SECRET_FILE_PATH = Path(secrets_file)
+    try:
+        with SECRET_FILE_PATH.open("r") as secret_file:
+            # Read the secret key from the file
+            secret = secret_file.read().strip()
+            if not secret:
+                raise ValueError("Secret file is empty")
+            return secret
+        
+    except FileNotFoundError:
+        # Let's create a cryptographically secure code in that file
+        with SECRET_FILE_PATH.open("w") as secret_file:
+            secret = secrets.token_hex(32)
+            secret_file.write(secret)
+            secret_file.flush()
+            secret_file.close()
+            return secret
