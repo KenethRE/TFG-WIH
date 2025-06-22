07ec46cfc1e90c77fcc1498d970efaf24379f176
Keneth Rodriguez <kenethlaunies.rodriguez@alu.uclm.es>
2025-06-22 18:00:03 +0000

    added bootstrap through javascript

--- www/v2/eventIntercept.js
+++ www/v2/eventIntercept.js
@@ -47,6 +47,18 @@ function socketSetup() {
 
     socket.on('registered', (data) => {
         console.log('Registered device ' + DEVICE_TYPE + ' with User ID ' + data.username);
+        let head = document.head;
+        let link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css';
+        link.integrity = 'sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC';
+        link.crossOrigin = 'anonymous';
+        head.appendChild(link);
+        let script = document.createElement('script');
+        script.src = 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js';
+        script.integrity = 'sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM';
+        script.crossOrigin = 'anonymous';
+        head.appendChild(script);
         captureEvents(data.event_list);
         console.log('Device Connected: ' + JSON.stringify(data.deviceinfo));
         login_text = document.getElementById('floating-login');        
